dae32768bbe2cd223e39aeef31cc8ba02af7bc6d
JoseFranciscoGomezMoreno <frankiski1998@hotmail.com>
2020-11-03 22:43:36 +0100

    .

---
+++ Practica 3.6/lab36.php
@@ -0,0 +1,9 @@
+<?php
+print "Su nombre es $_GET[nombre].</br>";
+print "Sus apellidos son $_GET[apellidos].</br>";
+print "Su edad es $_GET[edad].</br>";
+print "Su peso es $_GET[peso].</br>";
+print "Su sexo es $_GET[hm].</br>";
+print "Su estado civil es $_GET[abc].</br>";
+print "Sus aficiones son $_GET[edf].</br>";
+?>
