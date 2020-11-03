654adb7dd5996d3871160dd26b0e29e4f6015748
JoseFranciscoGomezMoreno <frankiski1998@hotmail.com>
2020-11-03 19:46:24 +0100

    .

---
+++ Practica3.3/Ejercicio.php
@@ -0,0 +1,16 @@
+<?php
+
+$n = 1;
+$n2 = (int) $n;
+$total = $n - $n2;
+print $total;
+
+if ($total > 0) {
+    print "Num decimal";
+}
+else{
+    print "Num sin decimal";
+}    
+//print is_numeric($n);
+//print is_float($n);
+?>
